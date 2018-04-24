3a66330b560b5a75ae1747f56d02b89f6fb78ad5
Achilleas Kyritsakas <akiritsakas@gmail.com>
2018-04-24 21:58:41 +0300

    refactor: add new action types

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,5 +1,6 @@
 export const NOTE_ARCHIVE = "NOTE_ARCHIVE";
 export const NOTE_SELECT = "NOTE_SELECT";
+export const NOTE_DESELECT = "NOTE_DESELECT";
 export const NOTE_NEW = "NOTE_NEW";
 export const NOTE_DELETE = "NOTE_DELETE";
 export const NOTE_CHANGE_TEXT = "NOTE_CHANGE_TEXT";
@@ -8,6 +9,7 @@ export const NOTES_FETCH_ERROR = "NOTES_FETCH_ERROR";
 export const NOTES_ADD = "NOTES_ADD";
 
 export const FOLDER_SELECT = "FOLDER_SELECT";
+export const FOLDER_DESELECT = "FOLDER_DESELECT";
 export const FOLDER_NEW = "FOLDER_NEW";
 export const FOLDER_EDIT = "FOLDER_EDIT";
 export const FOLDER_SAVE = "FOLDER_SAVE";
