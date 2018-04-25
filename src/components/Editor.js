c42767557350ca524b3251cc7323d54ba758185b
Achilleas Kyritsakas <akiritsakas@gmail.com>
2018-04-25 12:52:30 +0300

    feat: use DateWidget on Editor

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,33 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getActiveNote } from '../selectors/note';
-import { doChangeNoteText } from '../actions/note';
+import { doChangeNoteText, doDeselect } from '../actions/note';
+import DateWidget from './DateWidget';
 import style from './Editor.css';
 
-const getDate = (timestamp) => {
-  const date = new Date(timestamp);
-  return `${date.toDateString()} at ${date.toLocaleTimeString()}`;
-}
+class Editor extends Component {
+  // componentDidUpdate() {
+  //   if (this.textarea) {
+  //     this.textarea.focus();
+  //   }
+  // }
 
-const Editor = ({ note, onChange }) => (
-  <section className={style.editor}>
-    <div className={style.date}>{getDate(note.editedAt)}</div>
-    <textarea
-      className={style.textarea}
-      placeholder="Write your note"
-      value={note && note.text}
-      onChange={({ target }) => onChange(target.value)}
-    />
-  </section>
-);
+  render() {
+    const { note, onChange, onFocus } = this.props;
+
+    return (
+      <section className={style.editor}>
+        {(note && note.text) && (
+          <DateWidget
+            default={note.editedAt}
+            createdAt={note.createdAt}
+            editedAt={note.editedAt}
+          />
+        )}
+        {(note && note.text) && (
+          <textarea
+            className={style.textarea}
+            placeholder="Write your note"
+            value={note && note.text}
+            onChange={({ target }) => onChange(target.value)}
+            onFocus={onFocus}
+            ref={node => {
+              this.textarea = node;
+            }}
+          />
+        )}
+      </section>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   note: getActiveNote(state)
 });
 
 const mapDispatchToProps = dispatch => ({
-  onChange: value => dispatch(doChangeNoteText(value))
+  onChange: value => dispatch(doChangeNoteText(value)),
+  onFocus: () => dispatch(doDeselect())
 });
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(Editor);
